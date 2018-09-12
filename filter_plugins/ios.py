6170ecc3ad3dcdc02417a4a6f6230756a0282844
Luke Russell <lukerussell@hotmail.com.au>
2018-09-12 13:53:13 +1000

    Rollback moving tests.

--- filter_plugins/ios.py
+++ filter_plugins/ios.py
@@ -15,6 +15,36 @@ INTERFACE_NAMES = {
     'Gi': 'GigabitEthernet',
 }
 
+ROUTING_PROTOCOLS = {
+     'L': 'local',
+     'C': 'connected',
+     'S': 'static',
+     'R': 'rip',
+     'M': 'mobile',
+     'B': 'bgp',
+     'D': 'eigrp',
+     'EX': 'eigrp external',
+     'O': 'ospf',
+     'IA': 'ospf inter area',
+     'N1': 'ospf nssa external type 1',
+     'N2': 'ospf nssa external type 2',
+     'E1': 'ospf external type 1',
+     'E2': 'ospf external type 2',
+     'i': 'is-is',
+     'su': 'is-is summary',
+     'L1': 'is-is level-1',
+     'L2': 'is-is level-2',
+     'ia': 'is-is inter area',
+     '*': 'candidate default',
+     'U': 'per-user static route',
+     'o': 'odr',
+     'P': 'periodic downloaded static route',
+     'H': 'nhrp',
+     'l': 'lisp',
+     'a': 'application route',
+     '+': 'replicated route',
+     '%': 'next hop override',
+}
 
 def expand_interface_name(name):
     match = re.match('([a-zA-Z]*)', name)
@@ -23,12 +53,20 @@ def expand_interface_name(name):
         name = name.replace(matched, INTERFACE_NAMES[matched])
     return name
 
+def expand_routing_protocol_name(name):
+    match = re.match(r'([\S]*)', name)
+    if match and match.group(1) in ROUTING_PROTOCOLS:
+        matched = match.group(1)
+        name = name.replace(matched, ROUTING_PROTOCOLS[matched])
+    return name
+
 
 class FilterModule(object):
     """Filters for working with output from network devices"""
 
     filter_map = {
-        'expand_interface_name': expand_interface_name
+        'expand_interface_name': expand_interface_name,
+        'expand_routing_protocol_name': expand_routing_protocol_name
     }
 
     def filters(self):
